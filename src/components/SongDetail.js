1dafdd5192b40aa850b0b3a1be37b9e1d7497105
Janie Amero <janie.amero@gmail.com>
2019-04-18 18:08:48 +0200

    completed

---
+++ src/components/SongDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  console.log(song);
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.song.title}
+        <br />
+        Duration: {song.song.duration}
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
